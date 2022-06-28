f97e3ef8b711153642808219cd6100aacee03281
Nick Selvaggio <nick@segosolutions.com>
2022-06-27 22:07:13 -0400

    Hard coded ganalytics code

--- pages/_app.js
+++ pages/_app.js
@@ -5,24 +5,22 @@ import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
   return (
   <>
-    <Script strategy="lazyOnload" id="gtags" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+    <Script strategy="lazyOnload" id="gtags" src={`https://www.googletagmanager.com/gtag/js?id=G-Y327X005E3`} />
 
-<Script id="ganaltyics" strategy="lazyOnload">
-    {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-        page_path: window.location.pathname,
-        });
-    `}
-</Script>
-
-
-<Head>
-    <title>Welcome!</title>
-    <meta name="viewport" content="initial-scale=1, width=device-width" />
-</Head>
+    <Script id="ganaltyics" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Y327X005E3', {
+            page_path: window.location.pathname,
+            });
+        `}
+    </Script>
+    <Head>
+        <title>Welcome!</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+    </Head>
     <Component {...pageProps} />
   </>
   );
