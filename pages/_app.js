f361e2f622f1928d89b588592d1f57e7eb4af95d
Nick Selvaggio <nick@segosolutions.com>
2023-06-13 22:37:27 -0400

    Update google tag and fix link issue

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,14 @@ import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
   return (
   <>
-    <Script strategy="lazyOnload" id="gtags" src={`https://www.googletagmanager.com/gtag/js?id=G-Y327X005E3`} />
+    <Script strategy="lazyOnload" id="gtags" src={`https://www.googletagmanager.com/gtag/js?id=G-ZNEWL0M5SB`} />
 
     <Script id="ganaltyics" strategy="lazyOnload">
         {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-Y327X005E3', {
+            gtag('config', 'G-ZNEWL0M5SB', {
             page_path: window.location.pathname,
             });
         `}
--- pages/eth-staking.js
+++ pages/eth-staking.js
@@ -57,7 +57,7 @@ export default function Home() {
      
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://liblockchain.xyz"
           target="_blank"
           rel="noopener noreferrer"
         >
